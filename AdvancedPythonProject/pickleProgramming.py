ff0555328d5e4db3b988c236902b1b37848c298c
Varinder <vssandhu043@gmail.com>
2019-01-12 22:39:16 +0530

    saving changes from ubunutu

---
+++ AdvancedPythonProject/pickleProgramming.py
@@ -0,0 +1,4 @@
+# Pickle is used for object deserialization and serialization also known to marshal python objects
+# Pickle has various protocols ranging from protocol 0 to 4
+# With python 3.4 protocol 4 is generally used
+# Use this URL to read the docs - https://docs.python.org/3.5/library/pickle.html
