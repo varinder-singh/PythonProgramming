5292f76f30b479813dd529a68e5fdf4d9d61aad9
Varinder <vssandhu043@gmail.com>
2019-01-17 10:08:13 +0530

    adding more files in python and module

---
+++ ModuleAndFuntions/turtleImport.py
@@ -0,0 +1,17 @@
+import turtle  # to draw something on the screen
+
+import time  # to have the window sleep for the specified seconds
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+turtle.circle(50)
+#time.sleep(5)
+
+# can use done instead of time
+turtle.done()
