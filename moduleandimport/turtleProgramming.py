f8b0d173a73b5549668e8ee52ea716b43ae1a3de
Varinder <vssandhu043@gmail.com>
2019-01-18 14:09:06 +0530

    committing from windows

---
+++ moduleandimport/turtleProgramming.py
@@ -0,0 +1,11 @@
+# Turtle is used for canvas design
+# draws images using turtle api's
+
+import turtle
+
+turtle.right(45)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.done()
